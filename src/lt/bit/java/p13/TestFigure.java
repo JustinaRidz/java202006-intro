04653c06fb83b0f35fb016c3df8e35dd667d9b95
Justina <justina.ridz@gmail.com>
2020-06-29 22:29:08 +0300

    13. figuros

--- src/lt/bit/java/p13/TestFigure.java
+++ src/lt/bit/java/p13/TestFigure.java
@@ -3,7 +3,24 @@ package lt.bit.java.p13;
 public class TestFigure {
     public static void main(String[] args) {
 
-        Triangle figure = new Triangle("T1", 3, 4, 5);
-        System.out.println(figure.area());
+        Figure[] figures = new Figure[] {
+                new Triangle("T1", 3, 4, 5),
+                new Circle("C2", 4.5),
+                new Circle("C3", 10),
+                new Triangle("T2", 6, 7, 9)
+        };
+
+        double totalArea = 0;
+//        for (int i = 0; i < figures.length; i++) {
+//            Figure f = figures[i];
+        for (Figure f : figures) {
+            if (f instanceof Triangle) {
+                totalArea += Calculations.calculateTriangleArea((Triangle) f);
+            } else if (f instanceof Circle) {
+                totalArea += Calculations.calculateCircleArea((Circle) f);
+            }
+        }
+        System.out.println("Total area: " + totalArea);
+
     }
 }
